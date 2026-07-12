cc290bfe4ed8ba9a0ae24d7bd38a0cb05d35b6e5
Adithyan <adithyanthyaga@gmail.com>
2026-07-12 12:41:12 +0530

    Describe your changes

--- frontend/src/services/privateApi.js
+++ frontend/src/services/privateApi.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
 const privateApi = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 privateApi.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access");
+    const token = localStorage.getItem("access");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
 
-  return config;
+    return config;
 });
 
 export default privateApi;
--- frontend/src/services/publicApi.js
+++ frontend/src/services/publicApi.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
+console.log("ENV =", import.meta.env);
+console.log("API =", import.meta.env.VITE_API_URL);
+
 const publicApi = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
 });
